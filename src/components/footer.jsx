2e136415ebf5effe6089fed66462407401ba3489
gmalik83 <gaurav98malik@gmail.com>
2022-03-25 23:06:49 +0530

    Footer Added

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+// Defining Footer
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className="text-center py-3">Copyright &copy;gmalik83</Col>
+      </Row>
+    </Container>
+  );
+};
+export default Footer;
